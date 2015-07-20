c2782e4038eb1c8a6453ebde39450b4d1c0cc320
= <=>
2015-07-20 15:46:31 -0500

    Sign up page, posts page

---
+++ feels/index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel = "stylesheet" type = "text/css" href = "../css/feels.css">
+	</head>
+	<body>
+		<div id = "wrapper">
+		<div id = "header">
+			<h1>feel<span style = "color: blue;">r</span></h1>
+			<p>let it all out.</p>
+		</div> <!-- header -->
+			<div class = "feel">
+				<p>derpaderpaderpadfj;alskdfj</p>	
+				<div class = 'u_n'>-Anonymous</div>
+				<ul class = 'c_h'>
+					<li><div class = "comments">4,344 comments</div></li>
+					<li><div class = "heart"><img src = "../imgs/heart.png" width = 8px; height = 8px;></div></li>
+					<li><div class = 'h_c'>(34)</div></li>
+				</ul>
+			</div>
+			<div class = "feel">
+				<p>derpaderpaderpadfj;alskdfj</p>	
+				<div class = 'u_n'>-Anonymous</div>
+				<ul class = 'c_h'>
+					<li><div class = "comments">4,344 comments</div></li>
+					<li><div class = "heart"><img src = "../imgs/heart.png" width = 8px; height = 8px;></div></li>
+					<li><div class = 'h_c'>(34)</div></li>
+				</ul>
+			</div>
+			<div class = "feel">
+				<p>derpaderpaderpadfj;alskdfj</p>	
+				<div class = 'u_n'>-Anonymous</div>
+				<ul class = 'c_h'>
+					<li><div class = "comments">4,344 comments</div></li>
+					<li><div class = "heart"><img src = "../imgs/heart.png" width = 8px; height = 8px;></div></li>
+					<li><div class = 'h_c'>(34)</div></li>
+				</ul>
+			</div>
+			<div class = "feel">
+				<p>derpaderpaderpadfj;alskdfj</p>	
+				<div class = 'u_n'>-Anonymous</div>
+				<ul class = 'c_h'>
+					<li><div class = "comments">4,344 comments</div></li>
+					<li><div class = "heart"><img src = "../imgs/heart.png" width = 8px; height = 8px;></div></li>
+					<li><div class = 'h_c'>(34)</div></li>
+				</ul>
+			</div>	
+			</div>			
+		</div> <!-- wrapper -->
+	</body>
+</html>
