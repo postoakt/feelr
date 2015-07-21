7422ac043dadf0c7c20a192b1b6a4d5fa33703bd
= <=>
2015-07-20 19:07:06 -0500

    derp

--- feels/index.php
+++ feels/index.php
@@ -6,46 +6,36 @@
 	<body>
 		<div id = "wrapper">
 		<div id = "header">
-			<h1>feelr</h1>
+			<h1><a href = "../">feelr</a></h1>
 			<p>let it all out.</p>
+			<ul class = "nav">
+				<li><a href = "../"><div class = "log_in">log in</div></a></li>
+				<li><a href = "../signup"><div class = "sign_up">sign up</div></a></li>
+			</ul>
 		</div> <!-- header -->
-			<div class = "feel">
-				<p>derpaderpaderpadfj;alskdfj</p>	
+		<div id = "sort_menu">
+			<ul>
+				<li><a class = "active" href = "#">Latest</a></li>
+				<li><a href = "#">Popular Today</a></li>
+				<li><a href = "#">Popular This Week</a></li>
+				<li><a href = "#">Random</a></li>
+			</ul>
+		</div> <!-- sort menu -->
+		<?php
+		
+		for ($i = 0; $i < 10; $i++){	
+		
+			echo "<div class = 'feel'>
+				<p>afl;jasj;lfkdkjl;asfdjkl;safdjlk;fsajlk;djlk;asfdjkl;asfdjlk;sdafkl;fasdasdfasdfasdfafl;jasj;lfkdkjl;asfdjkl;safdjlk;fsajlk;djlk;asfdjkl;asfdjlk;sdafkl;fasdasdfasdfasdfafl;jasj;lfkdkjl;asfdjkl;safdjlk;fsajlk;djlk;asfdjkl;asfdjlk;sdafkl;fasdasdfasdfasdfafl;jasj;lfkdkjl;asfdjkdfdfdflasdfasdfasdfffffff</p>	
 				<div class = 'u_n'>-Anonymous</div>
 				<ul class = 'c_h'>
-					<li><div class = "comments">4,344 comments</div></li>
-					<li><div class = "heart"><img src = "../imgs/heart.png" width = 8px; height = 8px;></div></li>
+					<li><div class = 'comments'>4,344 comments</div></li>
+					<li><div class = 'heart'><img src = '../imgs/heart.png' width = 8px; height = 8px;></div></li>
 					<li><div class = 'h_c'>(34)</div></li>
 				</ul>
-			</div>
-			<div class = "feel">
-				<p>derpaderpaderpadfj;alskdfj</p>	
-				<div class = 'u_n'>-Anonymous</div>
-				<ul class = 'c_h'>
-					<li><div class = "comments">4,344 comments</div></li>
-					<li><div class = "heart"><img src = "../imgs/heart.png" width = 8px; height = 8px;></div></li>
-					<li><div class = 'h_c'>(34)</div></li>
-				</ul>
-			</div>
-			<div class = "feel">
-				<p>derpaderpaderpadfj;alskdfj</p>	
-				<div class = 'u_n'>-Anonymous</div>
-				<ul class = 'c_h'>
-					<li><div class = "comments">4,344 comments</div></li>
-					<li><div class = "heart"><img src = "../imgs/heart.png" width = 8px; height = 8px;></div></li>
-					<li><div class = 'h_c'>(34)</div></li>
-				</ul>
-			</div>
-			<div class = "feel">
-				<p>derpaderpaderpadfj;alskdfj</p>	
-				<div class = 'u_n'>-Anonymous</div>
-				<ul class = 'c_h'>
-					<li><div class = "comments">4,344 comments</div></li>
-					<li><div class = "heart"><img src = "../imgs/heart.png" width = 8px; height = 8px;></div></li>
-					<li><div class = 'h_c'>(34)</div></li>
-				</ul>
-			</div>	
-			</div>			
+			</div>";
+		}
+		?>						
 		</div> <!-- wrapper -->
 	</body>
 </html>
